a192f0d8b0c87110f01cfbc7dd266558e54c23f4
maxxam <minh00012258@gmail.com>
2020-03-11 23:56:57 +0700

    commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const port = 3000
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+//app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+app.get('/', (req, res) => res.send('hello'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
