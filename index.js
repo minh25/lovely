0a0489f109c65ffd1b6068394e95df71cdef8ee2
maxxam <minh00012258@gmail.com>
2020-03-13 10:14:11 +0700

    commit

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
-const app = express()
 const path = require('path')
-const port = 3000
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const app = express()
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
-// app.get('/', (req, res) => res.send('hello'))
+// // app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
+// // app.get('/', (req, res) => {
+// //     res.send('hello')
+// // })
+// // app.listen(port, () => console.log('Server ready'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// http.createServer((req, res) => {
+//     res.write('Hello World!');
+//     res.sendFile(path.join(__dirname + '/index.html'));
+//     res.end();
+// }).listen(8080);
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(3000);
+});
