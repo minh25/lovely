8e25700b7528783f8d53b47b62870978e3f87e0d
maxxam <minh00012258@gmail.com>
2020-03-13 16:07:02 +0700

    commit

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const fs = require('fs')
+const port = process.env.PORT || 5000
+const app = express()
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(process.env.PORT || 5000);
-});
+// fs.readFile('./index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(process.env.PORT || 5000);
+// });
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname + '/index.html'))
+)
+
+app.listen(port, () => console.log(`Listening on port ${port}!`))
